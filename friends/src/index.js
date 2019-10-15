a6c810eaa5b978ef11a2419ade3c3fa530b75c0f
seanaleid <seanaleid@gmail.com>
2019-10-15 15:32:57 -0400

    index.js is wrapped in the Provider and the store is configured

--- friends/src/index.js
+++ friends/src/index.js
@@ -4,7 +4,18 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// configure store
+import { Provider } from "react-redux";
+import configureStore from "./configureStore";
+
+const store = configureStore();
+console.log(`Index.js store console log`, store);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, 
+    document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
